fdcb082d037cb49df1ef798eb76e7cd3fca65421
exrat <francois.barcelo@gmail.com>
2019-07-24 20:06:52 +0200

    update filemanager

--- filemanager/conf.php
+++ filemanager/conf.php
@@ -4,25 +4,38 @@ $fm['tempdir'] = '/tmp';		// path were to store temporary data ; must be writabl
 $fm['mkdperm'] = 755; 		// default permission to set to new created directories
 
 // set with fullpath to binary or leave empty
-$pathToExternals['rar'] = '';
-$pathToExternals['zip'] = '';
-$pathToExternals['unzip'] = '';
-$pathToExternals['tar'] = '';
-$pathToExternals['gzip'] = '';
-$pathToExternals['bzip2'] = '';
+
+if(empty($pathToExternals['rar']))	// May be path already defined?
+{
+	$pathToExternals['rar'] = '';
+}
+if(empty($pathToExternals['zip']))	// May be path already defined?
+{
+	$pathToExternals['zip'] = '';
+}
+if(empty($pathToExternals['unzip']))	// May be path already defined?
+{
+	$pathToExternals['unzip'] = '';
+}
+if(empty($pathToExternals['tar']))	// May be path already defined?
+{
+	$pathToExternals['tar'] = '';
+}
+if(empty($pathToExternals['gzip']))	// May be path already defined?
+{
+	$pathToExternals['gzip'] = '';
+}
+if(empty($pathToExternals['bzip2']))	// May be path already defined?
+{
+	$pathToExternals['bzip2'] = '';
+}
 
 // archive mangling, see archiver man page before editing
 
 $fm['archive']['types'] = array('rar', 'zip', 'tar', 'gzip', 'bzip2');
 
-
-
-
 $fm['archive']['compress'][0] = range(0, 5);
 $fm['archive']['compress'][1] = array('-0', '-1', '-9');
 $fm['archive']['compress'][2] = $fm['archive']['compress'][3] = $fm['archive']['compress'][4] = array(0);
-
-
-
 
 ?>
